c205830589a612f1d42cfb8cc2d9fbc9d34ce697
Sunny Golovine <sunny@glvn.co>
2022-02-10 17:05:45 -0700

    Build out search hook

--- src/hooks/useIconSearch.ts
+++ src/hooks/useIconSearch.ts
@@ -1,8 +1,36 @@
 import { useState, useEffect } from "react"
 import siSlugs from "../../si-slugs.json"
 
-const iconMap = siSlugs.iconMap
+interface IconMapItem {
+  label: string
+  slug: string
+}
+
+const iconMap: IconMapItem[] = siSlugs.iconMap
 
 export const useIconSearch = () => {
-  const [searchValue, setSearchValue] = useState<boolean>(false)
+  const [searchValue, setSearchValue] = useState<string>("")
+  const [searchResults, setSearchResults] = useState<IconMapItem[]>([])
+
+  useEffect(() => {
+    if (searchValue) {
+      const results = iconMap.filter(item => {
+        const upperCase = new RegExp(searchValue.toUpperCase())
+        const lowerCase = new RegExp(searchValue.toLowerCase())
+        const regularCase = new RegExp(searchValue, "g")
+
+        return (
+          regularCase.test(item.label) ||
+          regularCase.test(item.slug) ||
+          lowerCase.test(item.label) ||
+          lowerCase.test(item.slug) ||
+          upperCase.test(item.label) ||
+          upperCase.test(item.slug)
+        )
+      })
+      setSearchResults(results)
+    }
+  }, [searchValue])
+
+  return [searchValue, setSearchValue, searchResults]
 }
